92442a59794fb43c67c426c0495871e385eee850
Ken-ty <quentinserveaux.pro@gmail.com>
2026-01-14 14:58:05 +0400

    feat: CRUD pour gérer les inscriptions

---
+++ src/Form/InscriptionType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Inscription;
+use App\Entity\events;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InscriptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('email')
+            ->add('telephone')
+            ->add('nombrePlaces')
+            ->add('createdAt', null, [
+                'widget' => 'single_text'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Inscription::class,
+        ]);
+    }
+}
